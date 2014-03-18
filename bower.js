3ba6e45ca4b25120aea19b2d25f60558c805f764
Jordan Ell <jordan.ell7@gmail.com>
2014-03-18 15:13:17 -0700

    Initial commit

---
+++ bower.js
@@ -0,0 +1,9 @@
+{
+  "name": "bootstrap-markdown",
+  "version": "2.3.1",
+  "main": ["./js/bootstrap-markdown.js"],
+  "dependencies": {
+    "bootstrap": 	"~3.1.1",
+    "markdown": 	"value", "~0.4.0"
+  }
+}
